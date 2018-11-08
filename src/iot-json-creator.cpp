402e35c900ad7a93f0d4f58297319f47368ec01f
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-08 20:26:03 +0100

    added lenght of jsontemplate

--- src/iot-json-creator.cpp
+++ src/iot-json-creator.cpp
@@ -2,13 +2,14 @@
 #include "Arduino.h"
 
 const char *jsonTemplate = "{\"type\":\"%s\", \"place\":\"%s\", \"sensorName\": \"%s\", \"isChangeEvt\": %s, \"value\": %s}";
+const int jsonTemplateLen = sizeof(jsonTemplate) / sizeof(char);
 const char *trueStr = "true";
 const char *falseStr = "false";
 
 char *bmw12::createJson(const char *type, const char *place, const char *sensorName, const char *value, const bool isChangeEvt)
 {
     const char *isChangeEvtStr = (isChangeEvt) ? trueStr : falseStr;
-    int lenght = strlen(type) + strlen(place) + strlen(sensorName) + strlen(isChangeEvtStr) + strlen(value);
+    int lenght = jsonTemplateLen + strlen(type) + strlen(place) + strlen(sensorName) + strlen(isChangeEvtStr) + strlen(value);
     char *outString = (char *)malloc(sizeof(char) * (lenght + 1));
     sprintf(outString, jsonTemplate, type, place, sensorName, isChangeEvtStr, value);
     return outString;
