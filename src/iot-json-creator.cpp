9b3a887743e28c737d94fb73841bc117fd2e46d7
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-08 20:12:52 +0100

    moved to namespace

--- src/iot-json-creator.cpp
+++ src/iot-json-creator.cpp
@@ -5,11 +5,11 @@ const char *jsonTemplate = "{\"type\":\"%s\", \"place\":\"%s\", \"sensorName\": 
 const char *trueStr = "true";
 const char *falseStr = "false";
 
-char *createJson(const char *type, const char *place, const char *sensorName, const bool isChangeEvt, const char *value)
+char *createJson(const char *type, const char *place, const char *sensorName, const char *value, const bool isChangeEvt)
 {
     const char *isChangeEvtStr = (isChangeEvt) ? trueStr : falseStr;
     int lenght = strlen(type) + strlen(place) + strlen(sensorName) + strlen(isChangeEvtStr) + strlen(value);
-    char *outString = (char*)malloc(sizeof(char) * (lenght + 1));
+    char *outString = (char *)malloc(sizeof(char) * (lenght + 1));
     sprintf(outString, jsonTemplate, type, place, sensorName, isChangeEvtStr, value);
     return outString;
 }
@@ -17,12 +17,12 @@ char *createJson(const char *type, const char *place, const char *sensorName, co
 char *createJson(const char *type, const char *place, const char *sensorName, const bool value, const bool isChangeEvt)
 {
     const char *valueStr = (value) ? trueStr : falseStr;
-    return createJson(type, place, sensorName, isChangeEvt, valueStr);
+    return createJson(type, place, sensorName, valueStr, isChangeEvt);
 }
 
 char *createJson(const char *type, const char *place, const char *sensorName, const float value, const bool isChangeEvt)
 {
     char valueStr[20];
     dtostrf(value, 20, 6, valueStr);
-    return createJson(type, place, sensorName, isChangeEvt, valueStr);
+    return createJson(type, place, sensorName, valueStr, isChangeEvt);
 }
