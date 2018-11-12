a1e2716cfb3d8b02adac4a4d650265aa7c319379
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-12 19:48:00 +0100

    added ardionoName

--- src/iot-json-creator.cpp
+++ src/iot-json-creator.cpp
@@ -1,29 +1,29 @@
 #include "iot-json-creator.h"
 #include "Arduino.h"
 
-const char *jsonTemplate = "{\"type\":\"%s\", \"place\":\"%s\", \"sensorName\": \"%s\", \"isChangeEvt\": %s, \"value\": %s}";
+const char *jsonTemplate = "{\"type\":\"%s\", \"arduinoName\":\"%s\", \"place\":\"%s\", \"sensorName\": \"%s\", \"isChangeEvt\": %s, \"value\": %s}";
 const int jsonTemplateLen = strlen(jsonTemplate);
 const char *trueStr = "true";
 const char *falseStr = "false";
 
-char *bmw12::createJson(const char *type, const char *place, const char *sensorName, const char *value, const bool isChangeEvt)
+char *bmw12::createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const char *value, const bool isChangeEvt)
 {
     const char *isChangeEvtStr = (isChangeEvt) ? trueStr : falseStr;
-    int lenght = jsonTemplateLen + strlen(type) + strlen(place) + strlen(sensorName) + strlen(isChangeEvtStr) + strlen(value);
+    int lenght = jsonTemplateLen + strlen(type) + strlen(arduinoName) + strlen(place) + strlen(sensorName) + strlen(isChangeEvtStr) + strlen(value);
     char *outString = (char *)malloc(sizeof(char) * (lenght + 1));
     sprintf(outString, jsonTemplate, type, place, sensorName, isChangeEvtStr, value);
     return outString;
 }
 
-char *bmw12::createJson(const char *type, const char *place, const char *sensorName, const bool value, const bool isChangeEvt)
+char *bmw12::createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const bool value, const bool isChangeEvt)
 {
     const char *valueStr = (value) ? trueStr : falseStr;
-    return bmw12::createJson(type, place, sensorName, valueStr, isChangeEvt);
+    return bmw12::createJson(type, arduinoName, place, sensorName, valueStr, isChangeEvt);
 }
 
-char *bmw12::createJson(const char *type, const char *place, const char *sensorName, const float value, const bool isChangeEvt)
+char *bmw12::createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const float value, const bool isChangeEvt)
 {
     char valueStr[20];
     dtostrf(value, 20, 6, valueStr);
-    return bmw12::createJson(type, place, sensorName, valueStr, isChangeEvt);
+    return bmw12::createJson(type, arduinoName, place, sensorName, valueStr, isChangeEvt);
 }
--- src/iot-json-creator.h
+++ src/iot-json-creator.h
@@ -3,9 +3,9 @@
 
 namespace bmw12
 {
-char *createJson(const char *type, const char *place, const char *sensorName, const char *value, const bool isChangeEvt = false);
-char *createJson(const char *type, const char *place, const char *sensorName, const float value, const bool isChangeEvt = false);
-char *createJson(const char *type, const char *place, const char *sensorName, const bool value, const bool isChangeEvt = false);
+char *createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const char *value, const bool isChangeEvt = false);
+char *createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const float value, const bool isChangeEvt = false);
+char *createJson(const char *type, const char *arduinoName, const char *place, const char *sensorName, const bool value, const bool isChangeEvt = false);
 } // namespace bmw12
 
 #endif
